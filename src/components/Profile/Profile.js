878603ce2511b526bee9246350c31c439ad6b575
Ismael Bernal <ismaelbernal83@gmail.com>
2019-10-17 00:00:28 +0200

    Todo implementado menos el paginado

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -27,10 +27,10 @@ export default class Profile extends Component {
    */
   componentDidMount() {
     // Chequeo sesion del contexto, si no existe redirijo a register
-    const session = this.context.session
+    const { session } = this.context
     if (!session) {
       return this.props.history.push('/register');
-    } 
+    }
   }
 
   /**
@@ -40,70 +40,72 @@ export default class Profile extends Component {
     return (
       <UserConsumer.Consumer>
         { props => { 
-          return <div className='Login'>
-                  <div className='Login__Wrapper'>
-                    <form className='Login__Form'>
-                      <img src='https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1' className='Login__Logo' alt='nodepop-logo' />
-                      <FormControl>
-                        <Input
-                          name='name'
-                          value={props.session.name}
-                          readOnly
-                          type='text' 
-                          startAdornment={
-                            <InputAdornment position='start' className='InputIcon-icon'>
-                              <AccountCircleIcon/>
-                            </InputAdornment>
-                          }
-                          endAdornment={this.props.endAdornment}
-                        />
-                      </FormControl>
-                      <FormControl>
-                        <Input
-                          name='surname'
-                          value={props.session.surname}
-                          type='text' 
-                          readOnly
-                          startAdornment={
-                            <InputAdornment position='start' className='InputIcon-icon'>
-                              <AccountCircleIcon/>
-                            </InputAdornment>
-                          }
-                          endAdornment={this.props.endAdornment}
-                        />
-                      </FormControl>
-                      <FormControl>
-                        <Input
-                          name='birthday'
-                          value={props.session.birthday || ''}
-                          type='date' 
-                          readOnly
-                          startAdornment={
-                            <InputAdornment position='start' className='InputIcon-icon'>
-                              <CakeIcon/>
-                            </InputAdornment>
-                          }
-                          endAdornment={this.props.endAdornment}
-                        />
-                      </FormControl>
-                      <FormControl>
-                        <Input 
-                          name='api_key'
-                          value={props.session.api_key}
-                          placeholder='type your api key' 
-                          type='text'
-                          readOnly
-                          startAdornment={
-                            <InputAdornment position='start' className='InputIcon-icon'>
-                              <LockOpenIcon />
-                            </InputAdornment>
-                          }
-                        />
-                      </FormControl>
-                      <Button className='button' variant='contained' color='primary' onClick={this.handleDelete}> Delete data </Button>
-                    </form>
-                  </div>
-                </div>
+            if (props.session) {
+              return <div className='Login'>
+                      <div className='Login__Wrapper'>
+                        <form className='Login__Form'>
+                          <img src='https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1' className='Login__Logo' alt='nodepop-logo' />
+                          <FormControl>
+                            <Input
+                              name='name'
+                              value={props.session.name}
+                              readOnly
+                              type='text' 
+                              startAdornment={
+                                <InputAdornment position='start' className='InputIcon-icon'>
+                                  <AccountCircleIcon/>
+                                </InputAdornment>
+                              }
+                              endAdornment={this.props.endAdornment}
+                            />
+                          </FormControl>
+                          <FormControl>
+                            <Input
+                              name='surname'
+                              value={props.session.surname}
+                              type='text' 
+                              readOnly
+                              startAdornment={
+                                <InputAdornment position='start' className='InputIcon-icon'>
+                                  <AccountCircleIcon/>
+                                </InputAdornment>
+                              }
+                              endAdornment={this.props.endAdornment}
+                            />
+                          </FormControl>
+                          <FormControl>
+                            <Input
+                              name='birthday'
+                              value={props.session.birthday || ''}
+                              type='date' 
+                              readOnly
+                              startAdornment={
+                                <InputAdornment position='start' className='InputIcon-icon'>
+                                  <CakeIcon/>
+                                </InputAdornment>
+                              }
+                              endAdornment={this.props.endAdornment}
+                            />
+                          </FormControl>
+                          <FormControl>
+                            <Input 
+                              name='api_key'
+                              value={props.session.api_key}
+                              placeholder='type your api key' 
+                              type='text'
+                              readOnly
+                              startAdornment={
+                                <InputAdornment position='start' className='InputIcon-icon'>
+                                  <LockOpenIcon />
+                                </InputAdornment>
+                              }
+                            />
+                          </FormControl>
+                          <Button className='button' variant='contained' color='primary' onClick={this.handleDelete}> Delete data </Button>
+                        </form>
+                      </div>
+                    </div>
+            }
         }}
       </UserConsumer.Consumer>
     );
